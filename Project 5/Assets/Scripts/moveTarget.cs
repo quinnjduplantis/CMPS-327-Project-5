ad3569f04a4341b99fb7c3b5cbcd7f11d58bf847
QuinnJDuplantis <quinnjduplantis@gmail.com>
2017-11-20 11:32:02 -0600

    Project 5 from home added

---
+++ Project 5/Assets/Scripts/moveTarget.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveTarget : MonoBehaviour {
+
+    public GameObject target;
+    Vector3 targetPosition;
+
+	// Use this for initialization
+	void Start () {
+        targetPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                targetPosition = hit.point;
+                target.transform.position = targetPosition;
+            }
+        }
+	}
+}
